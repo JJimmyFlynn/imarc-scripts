cf9f4b0885da68ef2ef7776174c2f33337d715ca
John Flynn <jjimmyflynn@gmail.com>
2020-10-02 16:19:26 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Command } = require('commander')
+const CreateSassComponent = require('./scripts/create-sass-component.js')
+require('dotenv').config()
+
+const program = new Command
+
+program.version('1.0.0')
+    .command('create-sass-component')
+    .action((env, options) => {
+        CreateSassComponent()
+    })
+
+program.parse()
---
+++ scripts/create-sass-component.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+const slugify = require('slugify')
+
+const command = () => {
+    const sassDirectory = process.env.SASS_DIRECTORY || 'resources/styles'
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'type',
+            message: 'Create which type of component?',
+            choices: [
+                {
+                    name: 'Atom',
+                    value: 'atoms'
+                },
+                {
+                    name: 'Molecule',
+                    value: 'molecules'
+                },
+                {
+                    name: 'Organism',
+                    value: 'organisms'
+                }
+            ]
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Provide a component name (e.g. image-row)'
+        },
+        {
+            type: 'confirm',
+            name: 'createJs',
+            message: 'Create a JS file for the component?',
+            default: false
+        }
+    ]).then((answers) => {
+        const componentName = slugify(answers.name)
+        const componentType = answers.type
+        const componentTypePath = `./${sassDirectory}/${componentType}`
+        const componentPath = `${componentTypePath}/${componentName}`
+
+        if(!fs.existsSync(componentTypePath)) {
+            fs.mkdirSync(componentTypePath)
+        }
+
+        fs.mkdirSync(componentPath)
+        fs.writeFile(`${componentPath}/${componentName}.scss`, '', (error) => {
+            if (error) throw error
+        })
+        fs.writeFile(`${componentPath}/${componentName}.twig`, '', (error) => {
+            if (error) throw error
+        })
+
+        if(answers.createJs) {
+            fs.writeFile(`${componentPath}/${componentName}.js`, '', (error) => {
+                if (error) throw error
+            })
+        }
+
+    }).catch((error) => {
+        console.error(error)
+    })
+}
+
+module.exports = command
