6bec6e46c194612028dce8a84df60fbeb2c8a348
John Flynn <jjimmyflynn@gmail.com>
2020-10-08 11:59:21 -0400

    Add eslint

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const { Command } = require('commander')
 const CreateSassComponent = require('./scripts/create-sass-component.js')
 require('dotenv').config()
 
-const program = new Command
+const program = new Command()
 
 program.version('1.0.0')
-    .command('create-sass-component')
-    .action((env, options) => {
-        CreateSassComponent()
-    })
+  .command('create-sass-component')
+  .action((env, options) => {
+    CreateSassComponent()
+  })
 
 program.parse()
--- scripts/create-sass-component.js
+++ scripts/create-sass-component.js
@@ -3,70 +3,69 @@ const inquirer = require('inquirer')
 const slugify = require('slugify')
 
 const command = () => {
-    const sassDirectory = process.env.SASS_DIRECTORY || 'resources/styles'
-    inquirer.prompt([
+  const sassDirectory = process.env.SASS_DIRECTORY || 'resources/styles'
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'Create which type of component?',
+      choices: [
         {
-            type: 'list',
-            name: 'type',
-            message: 'Create which type of component?',
-            choices: [
-                {
-                    name: 'Atom',
-                    value: 'atoms'
-                },
-                {
-                    name: 'Molecule',
-                    value: 'molecules'
-                },
-                {
-                    name: 'Organism',
-                    value: 'organisms'
-                }
-            ]
+          name: 'Atom',
+          value: 'atoms'
         },
         {
-            type: 'input',
-            name: 'name',
-            message: 'Provide a component name (e.g. image-row)'
+          name: 'Molecule',
+          value: 'molecules'
         },
         {
-            type: 'confirm',
-            name: 'createJs',
-            message: 'Create a JS file for the component?',
-            default: false
+          name: 'Organism',
+          value: 'organisms'
         }
-    ]).then((answers) => {
-        const componentName = slugify(answers.name)
-        const componentType = answers.type
-        const componentTypePath = `./${sassDirectory}/${componentType}`
-        const componentPath = `${componentTypePath}/${componentName}`
+      ]
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Provide a component name (e.g. image-row)'
+    },
+    {
+      type: 'confirm',
+      name: 'createJs',
+      message: 'Create a JS file for the component?',
+      default: false
+    }
+  ]).then((answers) => {
+    const componentName = slugify(answers.name)
+    const componentType = answers.type
+    const componentTypePath = `./${sassDirectory}/${componentType}`
+    const componentPath = `${componentTypePath}/${componentName}`
 
-        if(!fs.existsSync(componentTypePath)) {
-            fs.mkdirSync(componentTypePath)
-        }
+    if (!fs.existsSync(componentTypePath)) {
+      fs.mkdirSync(componentTypePath)
+    }
 
-        fs.mkdirSync(componentPath)
-        fs.writeFile(`${componentPath}/${componentName}.scss`,
+    fs.mkdirSync(componentPath)
+    fs.writeFile(`${componentPath}/${componentName}.scss`,
             `/*
 /* General/Mobile
 /**********************************************************************/`,
             (error) => {
-            if (error) throw error
+              if (error) throw error
             }
-        )
-        fs.writeFile(`${componentPath}/${componentName}.twig`, '', (error) => {
-            if (error) throw error
-        })
-
-        if(answers.createJs) {
-            fs.writeFile(`${componentPath}/${componentName}.js`, '', (error) => {
-                if (error) throw error
-            })
-        }
-
-    }).catch((error) => {
-        console.error(error)
+    )
+    fs.writeFile(`${componentPath}/${componentName}.twig`, '', (error) => {
+      if (error) throw error
     })
+
+    if (answers.createJs) {
+      fs.writeFile(`${componentPath}/${componentName}.js`, '', (error) => {
+        if (error) throw error
+      })
+    }
+  }).catch((error) => {
+    console.error(error)
+  })
 }
 
 module.exports = command
